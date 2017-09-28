905e30494c15f81be4af1dff12b54f908e1cd060
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-28 17:11:43 +0100

    Added interface file"

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -1,6 +1,6 @@
 'use strict';
 
-describe('Thermostat', function() {
+describe('Thermostat does the following', function() {
 
   var thermostat;
 
@@ -8,21 +8,21 @@ describe('Thermostat', function() {
     thermostat = new Thermostat();
   });
 
-  it('starts at 20 degrees', function() {
+  it('starts at default temperature', function() {
   expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
 
-  it('increases in temperature with up()', function() {
+  it('increases in temperature with up() method', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
-  it('decreases in temperature with down()', function() {
+  it('decreases in temperature with down() method', function() {
     thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 
-  it('has a minimum of 10 degrees', function() {
+  it('has a minimum temperature of 10 degrees', function() {
     for (var i = 0; i < 11; i++) {
       thermostat.down();
     }
@@ -62,7 +62,7 @@ describe('when power saving mode is on', function() {
     thermostat = new Thermostat();
   });
 
-  it('has a maximum temperature of 25', function() {
+  it('thermostat has a maximum temperature of 25', function() {
       for (var i = 0; i < 6; i++) {
       thermostat.up();
       }
@@ -86,31 +86,26 @@ describe('when power saving mode is off', function() {
   });
 });
 
-describe('displaying usage levels', function() {
-  
+describe('Thermostat can also display energy usage levels', function() {
   var thermostat;
-
   beforeEach(function() {
     thermostat = new Thermostat();
   });
-
-  describe('when the temperature is below 18 degrees', function() {
-    it('it is considered low-usage', function() {
+  describe('when the temperature is below 18 degrees it', function() {
+    it('is considered low-usage', function() {
       for(var i = 0; i < 3; i++) {
         thermostat.down();
       }
       expect(thermostat.energyUsage()).toEqual('low-usage');
     });
   });
-
-  describe('when the temperature is between 18 and 25 degrees', function() {
-    it('it is considered medium-usage', function() {
+  describe('when the temperature is between 18 and 25 degrees it', function() {
+    it('is considered medium-usage', function() {
       expect(thermostat.energyUsage()).toEqual('medium-usage');
     });
   });
-
-  describe('when the temperature is above 25 degrees', function() {
-    it('it is considered high-usage', function() {
+  describe('when the temperature is above 25 degrees it', function() {
+    it('is considered high-usage', function() {
       thermostat.powerSavingMode = false;
       for(var i = 0; i < 6; i++) {
         thermostat.up();
