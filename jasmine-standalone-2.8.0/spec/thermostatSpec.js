9859a8ceb1760126974f6b00c24e88607f81053f
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-28 12:48:19 +0100

    Add thermostat.down method and test

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -2,17 +2,23 @@
 
 describe('Thermostat', function() {
 
-    var thermostat;
+  var thermostat;
 
-    beforeEach(function() {
-      thermostat = new Thermostat();
-    });
-
-    it('starts at 20 degrees', function() {
-    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  beforeEach(function() {
+    thermostat = new Thermostat();
   });
+
+  it('starts at 20 degrees', function() {
+  expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
   it('increases in temperature with up()', function() {
     thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
+
+  it('decreases in temperature with down()', function() {
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
   });
 });
