be0e910f88e66d3a6ec94ff4b206d9117e06f3b9
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-28 17:30:36 +0100

    Added jquery functions for increasing and decreasing temperature

--- jasmine-standalone-2.8.0/interface.js
+++ jasmine-standalone-2.8.0/interface.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+
   $('#temperature').text(thermostat.temperature);
-})
+
+  $('#temperature-up').click(function() {
+    thermostat.up();
+    $('#temperature').text(thermostat.temperature);
+});
+$('#temperature-down').click(function() {
+  thermostat.down();
+  $('#temperature').text(thermostat.temperature);
+});
+
+});
