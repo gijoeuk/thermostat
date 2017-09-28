db871ef9eb231ff87f7b7aa3a1b5d2dd71eb38e0
Joe Ingram <josephcingram@hotmail.co.uk>
2017-09-28 12:44:51 +0100

    Added functions to increase temperature. Test passing

--- jasmine-standalone-2.8.0/spec/thermostatSpec.js
+++ jasmine-standalone-2.8.0/spec/thermostatSpec.js
@@ -9,6 +9,10 @@ describe('Thermostat', function() {
     });
 
     it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+  it('increases in temperature with up()', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 });
